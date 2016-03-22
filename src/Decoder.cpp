bf042cb1adfa44f0d5da6b47b66fa774d96f903e
Victor Bucataru <ping@devktor.com>
2016-03-22 22:05:22 +0200

    primitive types decoder

---
+++ src/Decoder.cpp
@@ -0,0 +1,119 @@
+#include "Decoder.hpp"
+
+namespace Ethereum{namespace ABI{
+
+
+std::string Decoder::decodeString(const std::string &input) const
+{
+    return decodeString(input.data(), input.size());
+}
+
+
+std::string Decoder::decodeString(const char *input, size_t size) const
+{
+    if(size < 64)
+    {
+        throw std::runtime_error("invalid string data");
+    }
+
+    uint256_t strSize = decodeUint(input, 64);
+
+    if((size - 64) < strSize)
+    {
+        throw std::runtime_error("buffer overlflow");
+    }
+
+    std::string result;
+    strSize /= 2;
+    result.resize(strSize.template convert_to<int>());
+
+    boost::algorithm::unhex(input+64, input+size, result.begin());
+    return result;
+}
+
+
+uint256_t Decoder::decodeUint(const std::string &input) const
+{
+    if(input.size() < 64)
+    {
+        throw std::runtime_error("invalid string data");
+    }
+    return uint256_t(input);
+}
+
+
+uint256_t Decoder::decodeUint(const char *input, size_t size) const
+{
+    if(size < 64)
+    {
+        throw std::runtime_error("invalid string data");
+    }
+    //ToDo: optimize, construct without another string
+    return uint256_t(std::string(input, size));
+}
+
+
+int256_t Decoder::decodeInt(const std::string &input) const
+{
+    if(input.size() < 64)
+    {
+        throw std::runtime_error("invalid string data");
+    }
+    return parseInt(int256_t(input));
+}
+
+
+int256_t Decoder::decodeInt(const char *input, size_t size) const
+{
+    if(size < 64)
+    {
+        throw std::runtime_error("invalid string data");
+    }
+    //ToDo: optimize, construct without another string
+    return parseInt(int256_t(std::string(input, size)));
+}
+
+
+decimal_t Decoder::decodeDecimal(const std::string &input) const
+{
+    if(input.size() < 64)
+    {
+        throw std::runtime_error("invalid string data");
+    }
+    return parseDecimal(decimal_t(input));
+}
+
+
+decimal_t Decoder::decodeDecimal(const char *input, size_t size) const
+{
+    if(size < 64)
+    {
+        throw std::runtime_error("invalid string data");
+    }
+    //ToDo: optimize, construct without another string
+    return parseDecimal(decimal_t(std::string(input, size)));
+}
+
+
+int256_t Decoder::parseInt(const int256_t &input) const
+{
+    /*ToDo: optimize check first bit instead */
+    if(input > int256_t("0xEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"))
+    {
+        int256_t result = int256_t("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF") - input;
+        --result;
+        return result;
+    }
+    return input;
+}
+
+
+decimal_t Decoder::parseDecimal(const decimal_t &input) const
+{
+    decimal_t result = input / pow(decimal_t(2), 128);
+    return result;
+}
+
+
+
+}}
---
+++ src/Decoder.hpp
@@ -0,0 +1,40 @@
+#pragma once 
+
+#include <string>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/algorithm/hex.hpp>
+
+#include "types.hpp"
+
+namespace Ethereum{namespace ABI{
+
+
+class Decoder
+{
+    public:
+
+        std::string decodeString(const std::string &) const;
+        std::string decodeString(const char *, size_t) const;
+
+        uint256_t decodeUint(const std::string &) const;
+        uint256_t decodeUint(const char *, size_t ) const;
+
+        int256_t decodeInt(const std::string &) const;
+        int256_t decodeInt(const char *, size_t) const;
+
+        decimal_t decodeDecimal(const std::string &) const;
+        decimal_t decodeDecimal(const char *, size_t) const;
+
+        bool decodeBool(const std::string &) const;
+        bool decodeBool(const char *, size_t) const;
+
+    private:
+        void assertWordSize(size_t ) const;
+        int256_t parseInt(const int256_t &) const;
+        decimal_t parseDecimal(const decimal_t &) const;
+
+};
+
+
+}}

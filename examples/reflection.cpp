4fd29107ad19acc3bda39f93ccc0d6328cd4d208
Victor Bucataru <ping@devktor.com>
2016-03-22 22:17:20 +0200

    using contract result decoding

--- examples/reflection.cpp
+++ examples/reflection.cpp
@@ -9,30 +9,44 @@ using namespace Ethereum::ABI;
 class DummyInvoker
 {
     public:
-        std::string operator()(const std::string &address, const std::string &code);
-        std::string operator()(const std::string &from, const std::string &address, const std::string &code);
+        std::string execute(const std::string &address, const std::string &code);
+        std::string execute(const std::string &from, const std::string &address, const std::string &code);
+        std::string call(const std::string &address, const std::string &code);
+        std::string call(const std::string &from, const std::string &address, const std::string &code);
 };
 
 
 int main()
 {
     Reflection<DummyInvoker> contract("0x000000000000000000", DummyInvoker());
-    std::cout<<"result : "<<contract.call("foo", ARGUMENTS(uint256_t(1), int256_t(-1), FIXED((const unsigned char *)"hello world", 11), std::string("hello world"), ((const unsigned char *)"hello world", 11)));
+    std::cout<<"result : "<<contract.execute("foo", ARGUMENTS(uint256_t(1), int256_t(-1), FIXED((const unsigned char *)"hello world", 11), std::string("hello world"), ((const unsigned char *)"hello world", 11)));
     std::cout<<std::endl;
-    std::cout<<"result : "<<contract.call("0xFFFFFFFFFFFFFFFFFF","foo", ARGUMENTS(uint256_t(1), int256_t(-1), FIXED((const unsigned char *)"hello world", 11), std::string("hello world"), ((const unsigned char *)"hello world", 11)));
+    std::cout<<"result : "<<contract.execute("0xFFFFFFFFFFFFFFFFFF","foo", ARGUMENTS(uint256_t(1), int256_t(-1), FIXED((const unsigned char *)"hello world", 11), std::string("hello world"), ((const unsigned char *)"hello world", 11)));
     std::cout<<std::endl;
     return 0;
 }
 
 
-std::string DummyInvoker::operator()(const std::string &address, const std::string &code)
+std::string DummyInvoker::execute(const std::string &address, const std::string &code)
 {
     std::cout<<"called ["<<address<<"] : "<<code<<std::endl<<std::flush;
     return "foobar";
 }
 
-std::string DummyInvoker::operator()(const std::string &from, const std::string &address, const std::string &code)
+std::string DummyInvoker::execute(const std::string &from, const std::string &address, const std::string &code)
 {
     std::cout<<"called ["<<address<<"] from ["<<from<<"] : "<<code<<std::endl<<std::flush;
     return "foobar";
+}
+
+std::string DummyInvoker::call(const std::string &address, const std::string &code)
+{
+    std::cout<<"called ["<<address<<"] : "<<code<<std::endl<<std::flush;
+    return "0x0000000000000000000000000000000000000000000000000000000000000001";
+}
+
+std::string DummyInvoker::call(const std::string &from, const std::string &address, const std::string &code)
+{
+    std::cout<<"called ["<<address<<"] from ["<<from<<"] : "<<code<<std::endl<<std::flush;
+    return "0x0000000000000000000000000000000000000000000000000000000000000001";
 }

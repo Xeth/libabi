76e1971d71cbfdfe581fb8155a9753c28f4bc718
Victor Bucataru <ping@devktor.com>
2016-03-22 19:37:41 +0200

    example using source address

--- examples/reflection.cpp
+++ examples/reflection.cpp
@@ -10,6 +10,7 @@ class DummyInvoker
 {
     public:
         std::string operator()(const std::string &address, const std::string &code);
+        std::string operator()(const std::string &from, const std::string &address, const std::string &code);
 };
 
 
@@ -17,6 +18,9 @@ int main()
 {
     Reflection<DummyInvoker> contract("0x000000000000000000", DummyInvoker());
     std::cout<<"result : "<<contract.call("foo", ARGUMENTS(uint256_t(1), int256_t(-1), FIXED((const unsigned char *)"hello world", 11), std::string("hello world"), ((const unsigned char *)"hello world", 11)));
+    std::cout<<std::endl;
+    std::cout<<"result : "<<contract.call("0xFFFFFFFFFFFFFFFFFF","foo", ARGUMENTS(uint256_t(1), int256_t(-1), FIXED((const unsigned char *)"hello world", 11), std::string("hello world"), ((const unsigned char *)"hello world", 11)));
+    std::cout<<std::endl;
     return 0;
 }
 
@@ -24,5 +28,11 @@ int main()
 std::string DummyInvoker::operator()(const std::string &address, const std::string &code)
 {
     std::cout<<"called ["<<address<<"] : "<<code<<std::endl<<std::flush;
+    return "foobar";
+}
+
+std::string DummyInvoker::operator()(const std::string &from, const std::string &address, const std::string &code)
+{
+    std::cout<<"called ["<<address<<"] from ["<<from<<"] : "<<code<<std::endl<<std::flush;
     return "foobar";
 }
